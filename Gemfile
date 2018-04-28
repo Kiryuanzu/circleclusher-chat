b2295e51c18aa947e4f34fa6a07c779732153eb4
kiryuanzu <anzu.nagato.1005@gmail.com>
2018-04-29 00:34:16 +0900

    crusher-chatのAPIが叩けるようにした(未作動)

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ end
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.5'
+gem 'bootstrap-sass', '3.3.7'
+gem 'pry'
 # Use mysql as the database for Active Record
 gem 'mysql2', '>= 0.3.18', '< 0.5'
 # Use Puma as the app server
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,15 +7,14 @@ class MessagesController < ApplicationController
     message = Message.create(message_params)
     message_query = message_params[:text]
     message_url_query = URI.escape(message_query)
-    headers = {
-      #Watson API Explorerから参照
+    headers = {"Accept" => "application/json", "Authorization" => "Basic NmJmM2FlMWYtYTMwOC00NGQ3LWE4YTgtZTg5NTEzMTc5NGQwOjRscVJoUXlGaVN4Vw=="
     } 
-    watsonmessage = HTTParty.get(#Watson API　Explorerからリクエスト用のURLを引っ張って来る, :headers => headers  )
+    watsonmessage = HTTParty.get("https://watson-api-explorer.mybluemix.net/discovery/api/v1/environments/#{ENV['WATSON_ENVIROMENT_ID']}/collections/#{ENV['WATSON_COLLECTION_ID']}/query?query=%#{message_url_query}&count=10&version=2018-03-05", :headers => headers  )
     if watsonmessage["results"].present?
       watson_text = watsonmessage["results"][0]["A"]
       Message.create!(text: watson_text, admin: true, room_id: room.id)
+      room.update(room_crushpoint: room.crushpoint + 10 )
     end
-    #room.Update(room_crush_point: room.crush_point + 10)といったように返って来る発言の好感度レベルによってクラッシュポイントが増えるようにして、30までいったらゲームオーバーという仕組みにする
     redirect_to room_path
 
   end

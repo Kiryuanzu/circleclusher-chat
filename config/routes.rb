c5ab90b481de3463e0530a277f72cb71f7b3a63f
kiryuanzu <anzu.nagato.1005@gmail.com>
2018-06-08 01:12:54 +0900

    遊び方追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'top#index'
+  get 'about', to: 'top#about', as: 'about'
   get 'rooms', to: 'rooms#show', as: 'room'
   get 'kakoi_rooms', to: 'kakoi_rooms#show', as: 'kakoi_room'
   resources :messages, only: [:new, :create, :destroy]

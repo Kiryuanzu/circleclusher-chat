c48f8919d53699b7156b81447c5bda035b8a3f48
kiryuanzu <anzu.nagato.1005@gmail.com>
2018-06-27 21:25:50 +0900

    ポイント数によって画像が書き換わる独自ヘルパーメソッドを作成した。チャット画面のCSSを大幅に改良した

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def crasherimage(room)
+    if room.crushpoint < 50
+      image_tag ("crusher.png"), :class => "crusher-image"
+    else
+      image_tag ("crusher-over.png"), :class => "crusher-image"
+    end
+  end
+
+  def kakoiimage(room)
+    if room.kakoipoint < 50 
+      image_tag ("kakoi.png"), :class => "crusher-image"
+    else
+      image_tag ("kakoi-over.png"), :class => "crusher-image"
+    end
+  end
 end
